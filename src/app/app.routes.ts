e88e377712ac484cf19a2670a246903bca228fc4
Akshay Ashok <akshayashok238@gmail.com>
2025-05-21 23:43:03 +0530

    navbar and future reusable components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CandidateGridComponent } from './components/candidate-grid/candidate-grid.component';
+import { EmployerComponent } from './components/employer/employer.component'; // <-- import
+import { PagesComponent } from './components/pages/pages.component'; // <-- import
+import { HelpComponent } from './components/help/help.component'; // <-- import
 import { authGuard } from './auth.guard';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -11,6 +14,10 @@ export const routes: Routes = [
     component: CandidateGridComponent,
     canActivate: [authGuard]
   },
+  {path : 'home',component : HomeComponent}, // home component
+  { path: 'employer', component: EmployerComponent }, // <-- new route
+  { path: 'pages', component: PagesComponent },       // <-- new route
+  { path: 'help', component: HelpComponent },         // <-- new route
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
