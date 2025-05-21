91ceb01eb5555082ec23205d378adc5f0d3802d7
akshayashok01 <akshayashok238@gmail.com>
2025-05-21 22:17:35 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'job-angular';
+
+  constructor(public authService:AuthService){}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CandidateGridComponent } from './components/candidate-grid/candidate-grid.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { 
+    path: 'candidates', 
+    component: CandidateGridComponent,
+    canActivate: [authGuard]
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
