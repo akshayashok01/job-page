2b7672dce105ed681d254fc8adf615bece7044f6
Akshay Ashok <akshayashok238@gmail.com>
2025-05-24 14:35:06 +0530

    Added except userlist and style

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,10 +3,11 @@ import { FilterComponent } from '../filter/filter.component';
 import { UserListComponent } from '../user-list/user-list.component';
 import { JoinComponent } from '../join/join.component';
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [FilterComponent,UserListComponent,JoinComponent,BreadcrumbComponent],
+  imports: [FilterComponent,UserListComponent,JoinComponent,BreadcrumbComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
